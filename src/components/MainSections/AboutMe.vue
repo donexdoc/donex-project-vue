75e77ace6392374243478b8b907f7d12c61c7c14
donex <donexdoc@gmail.com>
2024-04-01 19:09:36 +0300

    Расширен блок "обо мне"

--- src/components/MainSections/AboutMe.vue
+++ src/components/MainSections/AboutMe.vue
@@ -1,14 +1,51 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div id="aboutMe">
-    <h1>Обо мне</h1>
-    <p>
-      Consequat sint velit id enim veniam. Anim quis irure commodo dolor proident consectetur
-      eiusmod Lorem ut in esse. Reprehenderit est cupidatat adipisicing deserunt laboris adipisicing
-      eiusmod velit nisi consectetur incididunt enim ut dolor. Dolore quis tempor in enim cillum et
-      ea. Velit sint deserunt esse in fugiat laborum consequat ex labore veniam dolor. Ullamco
-      officia minim enim eu et ipsum eu exercitation excepteur id aute ad qui quis.
-    </p>
-  </div>
+  <v-container id="aboutMe" fluid class="bg-blue-darken-2 pa-xs-5 pa-sm-10 pa-md-16">
+    <v-container>
+      <v-row>
+        <v-col cols="12" xs="12" md="8" lg="6">
+          <h1 class="text-h4 mb-5">Обо мне</h1>
+          <p class="text-subtitle-1">
+            Я - Донков Александр, мне {{ age }}. <br />
+            Уже продолжительное время, я занимаюсь web-разрботкой и являюсь Fullstack специалистом.
+            Имею обширный опыт в разработке приложений разных уровней сложностей. От простых
+            лендингов, до больших сервисов контроля качества.
+          </p>
+          <br />
+          <p>
+            Разработкой web-приложений я занимаюсь еще со школьных времен. И активно развиваюсь в
+            этом направлении по сей день. Пишу монолиты, микросервисы, API, CRM системы. Развиваю
+            свои навыки как во Frontend разарботке, так и в Backend. <br />
+            Всегда стараюсь поддерживать актуальные знания, пробовать новые технологии и следить за
+            трендами. Это помогает профессионально развиваться и мотивирует на углубленное изучение
+            IT сферы.
+          </p>
+        </v-col>
+        <v-col cols="12" xs="12" md="4" lg="6" class="d-flex justify-center">
+          <v-img
+            :src="adatpiveImg"
+            alt="adaptive"
+            class="pa-xs-16 pa-sm-16"
+            max-width="320px"
+          ></v-img>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-container>
 </template>
+
+<script setup lang="ts">
+import adatpiveImg from '@/assets/adaptive.png'
+import { ref } from 'vue'
+
+const currentDay = new Date()
+const birthDay = new Date(1997, 8, 7)
+
+const yearsDif = currentDay.getFullYear() - birthDay.getFullYear()
+const monthsDif = currentDay.getMonth() - birthDay.getMonth()
+const dateDif = currentDay.getDate() - birthDay.getDate()
+const age = ref(yearsDif)
+
+if ((monthsDif === 0 && dateDif < 0) || monthsDif < 0) {
+  age.value--
+}
+</script>
