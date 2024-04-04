09fec2d618bdfc85e93d3e40ae76216b879a3542
donex <donexdoc@gmail.com>
2024-04-05 00:04:17 +0300

    Добавлено описание блока с блогом

--- src/components/MainSections/YTBlog.vue
+++ src/components/MainSections/YTBlog.vue
@@ -1,12 +1,38 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div id="ytBlog">
-    <h1>Блог на YouTube</h1>
-    <p>
-      Magna minim excepteur do officia sunt fugiat est et qui. Veniam deserunt ullamco id velit
-      adipisicing exercitation incididunt. Est duis dolore sit nulla et veniam anim aute
-      adipisicing.
-    </p>
-  </div>
+  <v-container id="ytBlog" fluid class="bg-grey-darken-4 pa-xs-5 pa-sm-10 pa-md-16">
+    <v-container>
+      <h1 class="text-h4 mb-5">Мой блог</h1>
+      <p class="text-subtitle-1">
+        В свободное от работы время, я веду свой блог в тематике информационных технологий.
+        Рассказываю о играх, занимаюсь фановым программированием, обозреваю различную технику. Обо
+        всем этом я рассказываю на своем канале на
+
+        <v-btn
+          variant="text"
+          color="red"
+          href="https://www.youtube.com/@DonExCode/videos"
+          target="__blank"
+          >youtube.com/@DonExCode</v-btn
+        >
+
+        <v-btn
+          variant="text"
+          color="yellow-darken-2"
+          href="https://dzen.ru/donexcode"
+          target="__blank"
+          >dzen.ru/donexcode</v-btn
+        >
+
+        <v-btn
+          variant="text"
+          color="blue-darken-2"
+          href="https://vk.ru/video/@donex_code"
+          target="__blank"
+          >vk.ru/video/@donex_code</v-btn
+        >
+      </p>
+    </v-container>
+  </v-container>
 </template>
